465754f3a772b0e7a0c3b6675759a87d60097b55
Adrian <adrberia@gmail.com>
2020-05-26 20:37:58 -0400

    Updates

---
+++ precache-manifest.1ba8fe43e735f25b9eae5d2b790e25f2.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27a2f46da830766a28e094ef302ff3a1",
+    "url": "/react-promote-artist-work-tour-latam/index.html"
+  },
+  {
+    "revision": "4cd6e32e89ebe5fee947",
+    "url": "/react-promote-artist-work-tour-latam/static/css/main.fbc71c80.chunk.css"
+  },
+  {
+    "revision": "9e3849675feb7e807465",
+    "url": "/react-promote-artist-work-tour-latam/static/js/2.9259d75a.chunk.js"
+  },
+  {
+    "revision": "4cd6e32e89ebe5fee947",
+    "url": "/react-promote-artist-work-tour-latam/static/js/main.38bbf07f.chunk.js"
+  },
+  {
+    "revision": "71bf1035f5bec5f9d664",
+    "url": "/react-promote-artist-work-tour-latam/static/js/runtime~main.97083d61.js"
+  },
+  {
+    "revision": "c7a592e97eac8feb1d5e3d846492e456",
+    "url": "/react-promote-artist-work-tour-latam/static/media/Countdown-avatar-pb.c7a592e9.png"
+  },
+  {
+    "revision": "137ce7b9ec00e7349f1290d8f5c11e26",
+    "url": "/react-promote-artist-work-tour-latam/static/media/back.137ce7b9.svg"
+  },
+  {
+    "revision": "a35c848a8456d8e12c9ccb5d4e9da0b8",
+    "url": "/react-promote-artist-work-tour-latam/static/media/background.a35c848a.png"
+  },
+  {
+    "revision": "f6a4a9bbf5c2bfbdcc9946323d2240ec",
+    "url": "/react-promote-artist-work-tour-latam/static/media/close.f6a4a9bb.svg"
+  },
+  {
+    "revision": "f1271705cd33ba95f0505070ddf4f883",
+    "url": "/react-promote-artist-work-tour-latam/static/media/discovery logo.f1271705.png"
+  },
+  {
+    "revision": "3ee6c4e5bb555f858cae95d04f62d7f7",
+    "url": "/react-promote-artist-work-tour-latam/static/media/discovery_logo_brazil.3ee6c4e5.svg"
+  },
+  {
+    "revision": "c35d23fa79f5139b116669becdc48754",
+    "url": "/react-promote-artist-work-tour-latam/static/media/email.c35d23fa.svg"
+  },
+  {
+    "revision": "98fec0a61d08019500515c02c6f6f01a",
+    "url": "/react-promote-artist-work-tour-latam/static/media/facebook.98fec0a6.svg"
+  },
+  {
+    "revision": "2b288c129bad0731e06c78caf4616358",
+    "url": "/react-promote-artist-work-tour-latam/static/media/favicon-brasil.2b288c12.ico"
+  },
+  {
+    "revision": "b18d6498cc986df1ddd4db33efc2d2a6",
+    "url": "/react-promote-artist-work-tour-latam/static/media/favicon.b18d6498.ico"
+  },
+  {
+    "revision": "c31f082c8aad988f4b1415acb8331960",
+    "url": "/react-promote-artist-work-tour-latam/static/media/icon_chevron_right.c31f082c.svg"
+  },
+  {
+    "revision": "2af972736100c4e41f18358bb8b00ee0",
+    "url": "/react-promote-artist-work-tour-latam/static/media/logo easy.2af97273.png"
+  },
+  {
+    "revision": "33b6f1a62ca9c683c4c13f330e3f49dd",
+    "url": "/react-promote-artist-work-tour-latam/static/media/logo einhell.33b6f1a6.png"
+  },
+  {
+    "revision": "d3d447a697e0a10d1f40092c9379a1e1",
+    "url": "/react-promote-artist-work-tour-latam/static/media/logo scotiabank.d3d447a6.png"
+  },
+  {
+    "revision": "c654e32654470a9ce1adf09fad4de02b",
+    "url": "/react-promote-artist-work-tour-latam/static/media/logo sinteplast.c654e326.png"
+  },
+  {
+    "revision": "fd49d05f186d1800fbefbdcc6fbf3814",
+    "url": "/react-promote-artist-work-tour-latam/static/media/menu_white.fd49d05f.svg"
+  },
+  {
+    "revision": "a92816f479d940e5a57692e831a6f8dd",
+    "url": "/react-promote-artist-work-tour-latam/static/media/preview.a92816f4.jpg"
+  },
+  {
+    "revision": "d73fa202e0d7a0260a2394aed63b531e",
+    "url": "/react-promote-artist-work-tour-latam/static/media/twitter.d73fa202.svg"
+  },
+  {
+    "revision": "36a4448e80fef611fe0fdd69d9f6bd98",
+    "url": "/react-promote-artist-work-tour-latam/static/media/whatsapp.36a4448e.svg"
+  }
+]);
